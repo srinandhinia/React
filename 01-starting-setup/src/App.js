7ed2a3bd125f02438cc532d2f5d43c27a1706aef
srinandhinia <srinandini.3696@gmail.com>
2023-09-26 19:05:32 -0500

    Added ids to expense objects

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -2,16 +2,19 @@ import ExpenseComponent from "./components/ExpenseComponent";
 function App() {
   const expenseItem = [
     {
+      id: "e1",
       date: new Date(2023, 9, 15),
       title: "Laptop",
       price: 800.05,
     },
     {
+      id: "e2",
       date: new Date(2022, 9, 9),
       title: "Car",
       price: 60000.0,
     },
     {
+      id: "e3",
       date: new Date(2022, 8, 25),
       title: "Television",
       price: 1200.0,
@@ -20,10 +23,21 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseComponent>
-        date = {expenseItem[0].date} title={expenseItem[0].title} price=
-        {expenseItem[0].price}{" "}
-      </ExpenseComponent>
+      <ExpenseComponent
+        date={expenseItem[0].date}
+        title={expenseItem[0].title}
+        price={expenseItem[0].price}
+      />
+      <ExpenseComponent
+        date={expenseItem[1].date}
+        title={expenseItem[1].title}
+        price={expenseItem[1].price}
+      />
+      <ExpenseComponent
+        date={expenseItem[2].date}
+        title={expenseItem[2].title}
+        price={expenseItem[2].price}
+      />
     </div>
   );
 }
--- 01-starting-setup/src/components/ExpenseComponent.js
+++ 01-starting-setup/src/components/ExpenseComponent.js
@@ -2,11 +2,11 @@ import "./ExpenseComponent.css";
 function ExpenseComponent(props) {
   return (
     <div className="expense-item">
-      <div className="expense-item__date">{props.date}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">${props.price}</div>
+      <div className="expense-item__price">{props.price}</div>
     </div>
   );
 }
