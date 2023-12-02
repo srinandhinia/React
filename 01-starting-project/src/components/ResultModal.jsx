aa33af4ce8bd4dea69d2005745ec4495a8c54b82
srinandhinia <srinandini.3696@gmail.com>
2023-12-02 16:33:32 -0600

    Refs&Portaks Final

--- 01-starting-project/src/components/ResultModal.jsx
+++ 01-starting-project/src/components/ResultModal.jsx
@@ -1,8 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, leftOverTime, onReset },
   ref
 ) {
+  const leftOverTimeInSeconds = (leftOverTime / 1000).toFixed(2);
+  const score = (1 - leftOverTimeInSeconds / targetTime).toFixed(2) * 100;
   const dialog = useRef();
   useImperativeHandle(ref, () => {
     return {
@@ -11,19 +14,29 @@ const ResultModal = forwardRef(function ResultModal(
       },
     };
   });
-  return (
+
+  // Generally modals are placed in html dom according to the place they are used. But to change the place of it in html dom, we use createPortal(jsx,location in dom)
+  return createPortal(
+    // if we want to close dialog box with esc key, add built-in onClose method like below.
+    // <dialog ref={dialog} className="result-modal" onClose={onReset}>
     <dialog ref={dialog} className="result-modal">
-      <h2>{result}</h2>
+      <h2>
+        {leftOverTimeInSeconds <= 0
+          ? "You Lost!"
+          : `Success!! Your score: ${score}`}
+      </h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>{" "}
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{leftOverTimeInSeconds} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 export default ResultModal;
