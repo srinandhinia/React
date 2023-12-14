3270759d7b393c969c14d3f7f8fe51719fe3b70c
srinandhinia <srinandini.3696@gmail.com>
2023-12-13 20:01:21 -0600

    Final Picking-Fav-Places App by avoiding side effects using useEffect and useCallBack hooks

---
+++ Side-Effects/src/App.jsx
@@ -0,0 +1,106 @@
+import { useRef, useState, useEffect, useCallback } from "react";
+
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
+
+const storageIds = JSON.parse(localStorage.getItem("Selected Places"));
+const storagePlaces = storageIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
+function App() {
+  const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [pickedPlaces, setPickedPlaces] = useState(storagePlaces);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
+
+  function handleStartRemovePlace(id) {
+    setModalOpen(true);
+    selectedPlace.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    setModalOpen(false);
+  }
+
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+
+    const storageIds =
+      JSON.parse(localStorage.getItem("Selected Places")) || [];
+    if (storageIds.indexOf(id) === -1)
+      localStorage.setItem(
+        "Selected Places",
+        JSON.stringify([id, ...storageIds])
+      );
+  }
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    );
+    setModalOpen(false);
+    const storageIds =
+      JSON.parse(localStorage.getItem("Selected Places")) || [];
+    localStorage.setItem(
+      "Selected Places",
+      JSON.stringify(storageIds.filter((id) => id != selectedPlace.current))
+    );
+  }, []);
+
+  return (
+    <>
+      <Modal open={isModalOpen}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={"Select the places you would like to visit below."}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Places"
+          fallbackText={"Sorting places by distance..."}
+          places={availablePlaces}
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
